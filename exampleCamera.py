42f2e2ad9672aa0520bafeb24b45e5383604f051
Harry HKUSAIL <Harry@example.com>
2023-07-24 16:44:53 +0800

    server2 websocket function adding

--- exampleCamera.py
+++ exampleCamera.py
@@ -2,7 +2,7 @@ import time
 from threading import Thread
 import socketio_camera
 
-camera_id = 7  #change this for each camera
+camera_id = 47  #change this for each camera
 socket_server = "ws://localhost:12000" #change the server address
 
 Socket9 = Thread(target = socketio_camera.connect_server,args=[camera_id,socket_server])
@@ -17,12 +17,13 @@ Socket9.start()
 #for example, sent alarm to server every 4 second.
 
 
-while True:
-    time.sleep(5)
-    # print('trigger yellow alarm')
+# while True:
+#     time.sleep(5)
+#     # print('trigger yellow alarm')
 
-    # socketio_camera.yellow_alarm()
-    # socketio_camera.red_alarm()
+#     socketio_camera.yellow_alarm()
+#     time.sleep(5)
+#     socketio_camera.red_alarm()
 
 
 
--- webSocket_panel_server2.py
+++ webSocket_panel_server2.py
@@ -3,7 +3,65 @@ import time
 import asyncio
 import websockets
 
-async def sender(websocket,message):        
+camera_list = {}#Data Example: {camera_1 : {id:1, state:'connected',type:'camera'},........}
+mobile_device_list = {} #Data Example: {Ejh3gs6d8SHG8 : {id:1, state:'connected',type:'mobile_device'},........}
+panel_list = {}#Data Example: {panel_1:<websocket object>}
+
+# sample message ==> {"event":"disconnection","data":{"id":1,"state":1,"type":1}}
+def get_updated_list():
+        data={
+        "camera":sorted(list(camera_list.values()),key=lambda dict: dict['id']),
+        "mobile_device":sorted(list(mobile_device_list.values()),key=lambda dict: dict['id']),
+        "panel":sorted(list(panel_list.values()),key=lambda dict: dict['id']),
+        }
+        return  data 
+
+
+async def register(websocket,data):
+    match data["type"]:
+        case "camera" :
+            camera_list[f"camera_{data['id']}"] = {"id":data["id"],"state":data["state"],"type":data["type"]}
+        case "mobile_device": 
+            mobile_device_list[f"mobile_device_{data['id']}"] = {"id":data["id"],"state":data["state"],"type":data["type"]}
+        case "panel":
+            panel_list[f"panel_{data['id']}"] = {"id":data["id"],"state":data["state"],"type":data["type"]}
+
+    try:
+        await websocket.send()
+    except BaseException as e:
+        print(e)
+    
+
+async def disconnect(data):
+     match data["type"]:
+        case "camera" :
+            camera_list.pop(f"camera_{data['id']}")
+        case "mobile_device":
+            mobile_device_list.pop(f"mobile_device_{data['id']}")
+        case "panel":
+            panel_list.pop(f"panel_{data['id']}")
+    
+
+
+async def yellow_alarm(data):
+    camera_list[f"camera_{data['id']}"]["state"] = "yellow_alarm"
+
+
+async def red_alarm(data):
+    camera_list[f"camera_{data['id']}"]["state"] = "red_alarm"
+
+
+async def event_action(message):
+    match message["event"]:
+        case "register": await register(message["data"])
+        case "disconnection": await disconnect()
+        case "yellow_alarm": await yellow_alarm()
+        case "red_alarm":await red_alarm()        
+        case "cancel_alarm":await cancel_alarm()
+        case "cancel_all_alarms": await cancel_all_alarm()  
+
+
+async def sender(websocket,message):
         try:
             await websocket.send(message)
         except BaseException as e:
@@ -11,13 +69,13 @@ async def sender(websocket,message):
 
 async def receiver(websocket):    
 
-    try:
         async for message in websocket:
-            # await asyncio.sleep(2)
-            print(message)
-            await sender(websocket,"@@ res from server @@")
-    except BaseException as e:
-        print("receiver error : ",e)
+            try:
+                # await asyncio.sleep(2)
+                print(message)
+                await sender(websocket,"@@ res from server @@")
+            except BaseException as e:
+                print("receiver error : ",e)
 
 
 async def check_closed(websocket):
@@ -27,7 +85,7 @@ async def check_closed(websocket):
 
 async def handler(websocket, path):
     print("### ",path," connected ###")
-    await asyncio.gather(sender(websocket,"connected"),receiver(websocket),check_closed(websocket))
+    await asyncio.gather(sender(websocket,"websocket connected"),receiver(websocket),check_closed(websocket))
 
 
 asyncio.get_event_loop().run_until_complete(websockets.serve(handler, '', 8889))
